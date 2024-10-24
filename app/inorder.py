f36f281b8db4448c85c8b0f513815744f0d713d8
fex <fex@gofynd.com>
2024-10-24 18:52:28 +0530

    fex-commit: Fix applied based on fex review suggestions

--- app/inorder.py
+++ app/inorder.py
@@ -1,9 +1,9 @@
 def inorder_traversal(root):
-    def traverse(node, result):
-        if node:
-            traverse(node.left, result)
-            result.append(node.value)
-            traverse(node.right, result)
     result = []
-    traverse(root, result)
+    def traverse(node):
+        if node:
+            traverse(node.left)
+            result.append(node.value)
+            traverse(node.right)
+    traverse(root)
     return result
